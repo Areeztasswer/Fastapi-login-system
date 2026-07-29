e289e931b80c48f166c0df816992e5bfb4209e53
Muhammad Areez Tasswer <areeztassawer@gmail.com>
2026-07-29 12:04:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+MONGO_URI = "mongodb+srv://areez1712_db_user:nVPtC61xpSQXI6y1@cluster1.fsqxslh.mongodb.net/ProductDB?retryWrites=true&w=majority&appName=Cluster1"
+
+client = MongoClient(MONGO_URI)
 
 db = client["ProductDB"]
 
 product_collection = db["products"]
-
-
 user_collection = db["users"]
